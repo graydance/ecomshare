722e305de3873c76ceb3b65ee102db463ce74b87
Bryce <brycezhang.cn@gmail.com>
2021-02-20 10:24:46 +0800

    feat:add iOS share

--- ios/Classes/SwiftEcomsharePlugin.swift
+++ ios/Classes/SwiftEcomsharePlugin.swift
@@ -1,47 +1,165 @@
 import Flutter
-import UIKit
+import FBSDKCoreKit
+import FBSDKShareKit
 
 public class SwiftEcomsharePlugin: NSObject, FlutterPlugin {
-  static let MEDIA_TYPE_IMAGE = "image";
-  static let MEDIA_TYPE_TEXT = "text";
-
-  static let CHANNEL_INSTAGRAM = "instagram";
-  static let CHANNEL_FACEBOOK = "facebook";
-  static let CHANNEL_FACEBOOK_GROUP = "facebook group";
-  static let CHANNEL_FACEBOOK_PAGE = "facebook page";
-  static let CHANNEL_FACEBOOK_MARKET_PLACE = "facebook market place";
-  static let CHANNEL_TWITTER = "twitter";
-
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ecomshare", binaryMessenger: registrar.messenger())
-    let instance = SwiftEcomsharePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if (call.method == "getSupportedChannels") {
-      // TODO
-      var list = [String]()
-      if ((call.arguments as? String) == SwiftEcomsharePlugin.MEDIA_TYPE_IMAGE) {
-          list.append(SwiftEcomsharePlugin.CHANNEL_FACEBOOK_MARKET_PLACE);
-      }
-      list.append(SwiftEcomsharePlugin.CHANNEL_INSTAGRAM)
-      list.append(SwiftEcomsharePlugin.CHANNEL_FACEBOOK)
-      list.append(SwiftEcomsharePlugin.CHANNEL_FACEBOOK_GROUP)
-      list.append(SwiftEcomsharePlugin.CHANNEL_FACEBOOK_MARKET_PLACE)
-      list.append(SwiftEcomsharePlugin.CHANNEL_TWITTER)
-      result(list)
-    } else if (call.method == "shareTo") {
-        if let args = call.arguments as? Dictionary<String, String> {
-            let mediaType = args["mediaType"]!;
-            let channel = args["channel"]!;
-            let content = args["content"]!;
-            // TODO
-            result(FlutterError.init(code:"TODO", message: mediaType + "-" + channel, details: content))
-        }
-    } else {
-        result(FlutterMethodNotImplemented)
+    
+    public enum MediaType: String {
+        case text, image, video
     }
-  }
+    
+    public enum Channel: String, CaseIterable {
+        case instagram
+        case facebook
+        case twitter
+        
+        var isSupported: Bool {
+            switch self {
+            case .instagram:
+                return canOpenInstagram()
+            case .facebook:
+                return canOpenFacebook()
+            case .twitter:
+                return canOpenTwitter()
+            }
+        }
+    }
+    
+    public enum FlutterMethod: String {
+        case getSupportedChannels, shareTo
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "ecomshare", binaryMessenger: registrar.messenger())
+        let instance = SwiftEcomsharePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let method = FlutterMethod(rawValue: call.method) else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        
+        switch method {
+        case .getSupportedChannels:
+            result(Channel.allCases.filter { $0.isSupported }.map { $0.rawValue })
+        case .shareTo:
+            guard let args = call.arguments as? [String: String],
+                  let channel = Channel(rawValue: args["channel"] ?? ""),
+                  let content = args["content"] else {
+                result(FlutterError(code: "arguments is invalid", message: "arguments is invalid", details: call.arguments))
+                return
+            }
+            
+            switch channel {
+            case .instagram:
+                shareToInstagram(imagePath: content, result: result)
+            case .facebook:
+                shareToFacebook(args: args, content: content, result: result)
+            case .twitter:
+                shareToTwitter(text: content, result: result)
+            }
+        }
+    }
+    
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions as? [UIApplication.LaunchOptionsKey: Any])
+        return true
+    }
+    
+    public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return ApplicationDelegate.shared.application(application, open: url, sourceApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue, annotation: [UIApplication.OpenURLOptionsKey.annotation])
+    }
+    
+    public func application(_ application: UIApplication, open url: URL, sourceApplication: String, annotation: Any) -> Bool {
+        return ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    // MARK: - Private
+    
+    private lazy var _shareInstagram = ShareInstagram()
+    
+    private static func canOpenFacebook() -> Bool {
+        return true
+    }
+    
+    private static func canOpenInstagram() -> Bool {
+        let url = URL(string: "instagram://app")!
+        return UIApplication.shared.canOpenURL(url)
+    }
+    
+    private static func canOpenTwitter() -> Bool {
+        let url = URL(string: "twitter://")!
+        return UIApplication.shared.canOpenURL(url)
+    }
+    
+    private func shareToFacebook(args: [String: String], content: String, result: @escaping FlutterResult) {
+        guard
+            let mediaType = MediaType(rawValue: args["mediaType"] ?? "") else {
+            result(FlutterError(code: "arguments is invalid", message: "arguments is invalid", details: "Facebook share require mediaType"))
+            return
+        }
+        let sharingContent: SharingContent = {
+            switch mediaType {
+            case .text:
+                let url = URL(fileURLWithPath: content)
+                let content = ShareLinkContent()
+                content.contentURL = url
+                return content
+            case .image:
+                let url = URL(fileURLWithPath: content)
+                let photo = SharePhoto(imageURL: url, userGenerated: true)
+                let content = SharePhotoContent()
+                content.photos = [photo]
+                return content
+            case .video:
+                let url = URL(fileURLWithPath: content)
+                let video = ShareVideo(videoURL: url)
+                let content = ShareVideoContent()
+                content.video = video
+                return content
+            }
+        }()
+        
+        guard let controller = UIApplication.shared.windows.first?.rootViewController else {
+            result(false)
+            return
+        }
+        let rst = ShareDialog(fromViewController: controller, content: sharingContent, delegate: nil).show()
+        result(rst)
+    }
+    
+    private func shareToInstagram(imagePath: String, result: @escaping FlutterResult) {
+        guard SwiftEcomsharePlugin.canOpenInstagram() else {
+            result(false)
+            return
+        }
+        guard let controller = UIApplication.shared.windows.first?.rootViewController else {
+            result(false)
+            return
+        }
+        guard let image = UIImage(contentsOfFile: imagePath) else {
+            result(false)
+            return
+        }
+        
+        _shareInstagram.postToFeed(image: image, caption: "", bounds: controller.view.bounds, view: controller.view)
+    }
+    
+    private func shareToTwitter(text: String, result: @escaping FlutterResult) {
+        if #available(iOS 10.0, *) {
+            let url = URL(string: "twitter://post?message=\(text)")!
+            guard UIApplication.shared.canOpenURL(url) else {
+                result(false)
+                return
+            }
+            UIApplication.shared.open(url, options: [:]) { isSuccess in
+                result(isSuccess)
+            }
+        } else {
+            let url = URL(string: "https://twitter.com/intent/tweet?text=\(text)")!
+            UIApplication.shared.openURL(url)
+        }
+    }
 }
